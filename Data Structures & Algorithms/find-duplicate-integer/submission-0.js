e4959823351c6dfa0892e55aef08f13da89d2773
mohd-uvais <66182730+mohd-uvais@users.noreply.github.com>
2026-08-03 22:05:21 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    findDuplicate(nums) {
+        let slow = 0;
+        let fast = 0;
+
+        while (true) {
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+
+            if (slow === fast) {
+                slow = 0;
+
+                while (slow !== fast) {
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+
+                return slow;
+            }
+        }
+    }
+}
